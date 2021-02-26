2cfba939a22ac0431511f5923c5643c0372d0f6b
Mebyus <31649307+Mebyus@users.noreply.github.com>
2021-02-26 14:24:49 +0400

    Parse support for AO3

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/mebyus/ffd
 go 1.15
 
 require (
-	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
+	golang.org/x/net v0.0.0-20210224082022-3d97a244fca7
 	golang.org/x/text v0.3.5
 )
--- resource/archiveofourown/parse.go
+++ resource/archiveofourown/parse.go
@@ -1,11 +1,55 @@
 package archiveofourown
 
 import (
+	"fmt"
 	"io"
 
+	"github.com/mebyus/ffd/document"
 	"github.com/mebyus/ffd/resource/fiction"
+	"golang.org/x/net/html"
 )
 
 func (t *ao3Tools) Parse(src io.Reader) (book *fiction.Book, err error) {
+	chapters, err := parseWhole(src)
+	if err != nil {
+		err = fmt.Errorf("Parsing piece: %v", err)
+		return
+	}
+	book = &fiction.Book{
+		Chapters: chapters,
+	}
+	return
+}
+
+func parseWhole(src io.Reader) (chapters []fiction.Chapter, err error) {
+	n, err := html.Parse(src)
+	if err != nil {
+		return
+	}
+	d := document.FromNode(n)
+
+	node := d.GetNodeById("chapters")
+	if node == nil {
+		err = fmt.Errorf("unable to find chapters container")
+		return
+	}
+
+	document.Detach(node)
+
+	allowed := map[string]bool{
+		"p":      true,
+		"i":      true,
+		"b":      true,
+		"table":  true,
+		"tr":     true,
+		"td":     true,
+		"em":     true,
+		"strong": true,
+	}
+	document.Flatten(node, allowed)
+	chapter := &fiction.Chapter{
+		Body: node,
+	}
+	chapters = []fiction.Chapter{*chapter}
 	return
 }
